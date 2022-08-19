d72345b94deeca424453346e75ca789cb4de4fc2
torre <gabritorredev@gmail.com>
2022-08-19 14:07:55 +0200

    Add day 7 part 2

--- day07/part2.rs
+++ day07/part2.rs
@@ -1,13 +1,16 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-static mut POSITIVE_BAGS: Vec<String> = Vec::new();
+static mut RESULT: i32 = 0;
 static mut MAIN_BAGS: Vec<String> = Vec::new();
+static mut STACK: Vec<i32> = Vec::new();
 
+// for the main explenation read the part 1
+// the only differences are that the numbers are not removed
 fn main() {
 	let file = File::open("input.txt").unwrap();
 	let reader = BufReader::new(file);
-	let mut result = 0;
+	let mut shiny_gold_index = 0;
 	let mut lines: Vec<String> = Vec::new();
 	for line in reader.lines() {
 		lines.push(line.unwrap());
@@ -19,13 +22,7 @@ fn main() {
 		lines[i] = lines[i].replace("bag.", "");
 		lines[i] = lines[i].replace("bag", "");
 		lines[i] = lines[i].replace(" contain ", ", ");
-		for j in 0..10 {
-			let mut number = " ".to_string();
-			number.push_str(&j.to_string());
-			lines[i] = lines[i].replace(&number, "");
-		}
 		lines[i].pop();
-//		println!("{:?}", lines[i]);
 	}
 	for line in lines.iter() {
 		let mut bag_name = "".to_string();
@@ -38,65 +35,75 @@ fn main() {
 			}
 		}
 		unsafe {
+			if bag_name == "shiny gold" {
+				shiny_gold_index = MAIN_BAGS.len();
+			}
 			MAIN_BAGS.push(bag_name.clone());
-//			println!("pushato {} in indice {}", bag_name, MAIN_BAGS.len()-1);
 		}
 	}
 
-	for i in 0..lines.len() {
-//		println!("{:?}", lines[i]);
-		result += find_shiny_gold(i, &lines, i);
+	find_shiny_gold(shiny_gold_index, &lines, shiny_gold_index);
+	unsafe {
+		println!("{}", RESULT);
 	}
-	println!("{result}");
 }
 
-fn find_shiny_gold(index: usize, lines: &Vec<String>, main_line_index: usize) -> i32 {
+// if the line contains "no other" stop the recursion (this is the only way to stop the recursion)
+// loop through each bag name of the line (excluding the first one)
+// get the number of the current bag
+// remove the number to get only the name of the bag
+// get the line number of the bag
+// save in a global stack the bag number
+// once the recursion stop i pop the last inserted number from the stack and multiply that number
+// for each remaining number in the stack
+// after moltiply all the number in the stack a sum the resutl in the RESULT global variable
+// i think that this program work thanks to a lot of lucky (lol), the stack doesn't return wrong
+// value because of the '1' doesn't change the result of a moltiplication, if the first bag_number
+// were 2 (or a number different than 1) the result will be completely broken
+fn find_shiny_gold(index: usize, lines: &Vec<String>, shiny_gold_index: usize) {
+	let mut bag_number: i32;
 	if lines[index].contains(&"no other".to_string()) {
-//		println!("no other! a {}", lines[index]);
-		return 0;
+		return ();
 	}
-	if lines[index].contains("shiny gold") && lines[index][..10] != "shiny gold".to_string() {
-		unsafe {
-			POSITIVE_BAGS.push(MAIN_BAGS[index].clone());
-		}
-//		println!("trovato shiny gold a {}", lines[index]);
-		unsafe {
-//			println!("POSITIVE_BAGS: {:?}", POSITIVE_BAGS);
-		}
-		return 1;
-	}
-	for (i, bag) in lines[index].split(" , ").enumerate() {
+	for (i, bag_numbered) in lines[index].split(" , ").enumerate() {
 		if i == 0 {
 			continue;
 		}
-//		println!("prova |{}|", bag);
-		unsafe {
-			if POSITIVE_BAGS.iter().position(|x| x == bag) != None {
-//				println!("trovato in positive");
-				return 1;
+
+		bag_number = bag_numbered.split(" ").next().unwrap().parse().unwrap();
+		let mut bag = "".to_string();
+		for j in 0..10 {
+			let mut del_number = "".to_string();
+			del_number.push_str(&j.to_string());
+			del_number.push_str(&" ".to_string());
+			bag = bag_numbered.replace(&del_number, "");
+			if bag != bag_numbered {
+				break;
 			}
 		}
-		let mut new_bag_index: usize = index;
+
+		let new_bag_index: usize;
 		unsafe {
-			if MAIN_BAGS.iter().position(|x| x == bag) != None {
-				new_bag_index = MAIN_BAGS.iter().position(|x| x == bag).unwrap();
-				if new_bag_index == main_line_index {
+			if MAIN_BAGS.iter().position(|x| x == &bag) != None {
+				new_bag_index = MAIN_BAGS.iter().position(|x| x == &bag).unwrap();
+				if new_bag_index == shiny_gold_index {
 					continue;
 				}
 			}
 			else {
-//				println!("dio cane");
 				continue;
 			}
 		}
-//		println!("prova linea {} ci dovrebbe essere {}", &new_bag_index, bag);
-		if find_shiny_gold(new_bag_index, lines, main_line_index) != 0 {
-			unsafe {
-				POSITIVE_BAGS.push(MAIN_BAGS[index].clone());
-				return 1;
+		unsafe {
+			STACK.push(bag_number);
+		}
+		find_shiny_gold(new_bag_index, lines, shiny_gold_index);
+		unsafe {
+			let mut leaf = STACK.pop();
+			for i in 0..STACK.len() {
+				leaf = Some(leaf.unwrap() * STACK[i]);
 			}
+			RESULT += leaf.unwrap();
 		}
 	}
-//	println!("hello!");
-	return 0;
 }
