7b125b298837f7d5d5f94a71e6fa4175802a655d
torre <gabritorredev@gmail.com>
2022-09-02 14:47:14 +0200

    Add day 12 part 1

---
+++ day12/part1.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::convert::TryInto;
+
+fn main() {
+	let file = File::open("input.txt").unwrap();
+	let reader = BufReader::new(file);
+	let mut result = 0;
+	let mut lines: Vec<String> = Vec::new();
+	for line in reader.lines() {
+		lines.push(line.unwrap());
+	}
+	let directions = ['E', 'N', 'W', 'S'];
+	let mut y_value = 0;
+	let mut x_value = 0;
+	let mut facing = 'E';
+
+	for line in lines.iter_mut() {
+		let action = line.remove(0);
+		let value: i32 = line.parse().unwrap();
+		do_actions(action, &value, &directions, &mut y_value, &mut x_value, &mut facing);
+		println!("x = {}; y = {}", x_value, y_value);
+	}
+	result = x_value.abs() + y_value.abs();
+	println!("{result}");
+}
+
+fn do_actions(action: char, value: &i32, directions: &[char;4], y_value: &mut i32, x_value: &mut i32, facing: &mut char) {
+	println!("prova {}{}", action, value);
+	match action {
+		'E' => *x_value += value,
+		'N' => *y_value += value,
+		'W' => *x_value -= value,
+		'S' => *y_value -= value,
+		'F' => do_actions(*facing, value, directions, y_value, x_value, facing),
+		'R' => {
+			let turns = value/90;
+			let mut index: i32 = directions.iter().position(|x| x == facing).unwrap().try_into().unwrap();
+			if index - turns < 0 {
+				index = (directions.len() - (turns - index) as usize).try_into().unwrap();
+			}
+			else{
+				index -= turns;
+			}
+			*facing = directions[index as usize];
+			println!("ora rivolto a {facing}");
+		},
+		'L' => {
+			let turns = value/90;
+			let mut index: i32 = directions.iter().position(|x| x == facing).unwrap().try_into().unwrap();
+			if index + turns > 3 {
+				let length: i32 = directions.len().try_into().unwrap();
+				index = length - (turns + index);
+				index = index.abs();
+			}
+			else{
+				index += turns;
+			}
+			*facing = directions[index as usize];
+			println!("ora rivolto a {facing}");
+		},
+		_ => todo!(),
+	}
+}
